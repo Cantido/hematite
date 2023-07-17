ddd9cf4fa6e1eb9ed6f01b1a555e9b3ef560c106
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-07-17 17:23:36 +0000

    Barely working API

---
+++ src/db.rs
@@ -0,0 +1,328 @@
+use actix::prelude::*;
+use base64::{engine::general_purpose, Engine as _};
+use cloudevents::*;
+use cloudevents::event::Event;
+use std::collections::BTreeMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufRead};
+use std::path::PathBuf;
+use anyhow::{Result, bail};
+
+pub struct DatabaseActor {
+    pub database: Database,
+}
+
+impl Actor for DatabaseActor {
+    type Context = Context<Self>;
+}
+
+#[derive(Message)]
+#[rtype(result = "Result<Option<Event>>")]
+pub struct Fetch(pub u64);
+
+#[derive(Message)]
+#[rtype(result = "Result<()>")]
+pub struct Append(pub Event, pub ExpectedRevision);
+
+impl Handler<Fetch> for DatabaseActor {
+    type Result = Result<Option<Event>>;
+
+    fn handle(&mut self, msg: Fetch, _ctx: &mut Context<Self>) -> Self::Result {
+        self.database.query(msg.0)
+    }
+}
+
+impl Handler<Append> for DatabaseActor {
+    type Result = Result<()>;
+
+    fn handle(&mut self, msg: Append, _ctx: &mut Context<Self>) -> Self::Result {
+        self.database.insert(msg.0, msg.1)
+    }
+}
+
+pub enum ExpectedRevision {
+    Any,
+    NoStream,
+    StreamExists,
+    Exact(u64),
+}
+
+pub struct Database {
+    file: File,
+    primary_index: BTreeMap<u64, u64>,
+    source_id_index: BTreeMap<(String, String), u64>
+}
+
+impl Database {
+    pub fn new(path: &PathBuf) -> Result<Self> {
+        let file = File::options()
+            .read(true)
+            .append(true)
+            .create(true)
+            .open(path)?;
+
+        Ok(Database {
+            file,
+            primary_index: BTreeMap::new(),
+            source_id_index: BTreeMap::new()
+        })
+    }
+
+    pub fn query(&mut self, rownum: u64) -> Result<Option<Event>> {
+        let row_offset = match self.primary_index.get(&rownum) {
+          Some(row_offset) => row_offset,
+          None => return Ok(None)
+        };
+        let _position = self
+            .file
+            .seek(io::SeekFrom::Start(*row_offset))
+            .expect("Cannot seek to rownum's row");
+
+          io::BufReader::new(&self.file)
+              .lines()
+              .next()
+              .transpose()
+              .map(|r| r.map(decode_event))
+              .map_err(|e| e.into())
+    }
+
+    pub fn insert(
+        &mut self,
+        event: Event,
+        expected_revision: ExpectedRevision,
+    ) -> Result<()> {
+        let revision_match: bool = match expected_revision {
+            ExpectedRevision::Any => true,
+            ExpectedRevision::NoStream => self.primary_index.last_key_value().is_none(),
+            ExpectedRevision::StreamExists => self.primary_index.last_key_value().is_some(),
+            ExpectedRevision::Exact(revision) => self
+                .primary_index
+                .last_key_value()
+                .map(|t| t.0)
+                .map_or(false, |r| r == &revision),
+        };
+
+        if revision_match {
+            self.write_event(event)
+        } else {
+            bail!("revision mismatch");
+        }
+    }
+
+    fn write_event(&mut self, event: Event) -> Result<()> {
+        if self.source_id_index.contains_key(&(event.source().to_string(), event.id().to_string())) {
+          bail!("Event with that source and ID value already exists in this stream");
+        }
+        let position = self.file.seek(io::SeekFrom::End(0))?;
+
+        let (event_rownum, event_offset) =
+          match self.primary_index.last_key_value() {
+              None => {
+                  (0, 0)
+              }
+              Some((last_rownum, _offset)) => {
+                  (last_rownum + 1, position)
+              }
+          };
+
+        let encoded = encode_event(&event)?;
+        self.file.write_all(&encoded)?;
+
+        self.primary_index.insert(event_rownum, event_offset);
+        self.source_id_index.insert((event.source().to_string(), event.id().to_string()), event_rownum);
+
+        Ok(())
+    }
+}
+
+fn encode_event(event: &Event) -> Result<Vec<u8>> {
+    let json = serde_json::to_string(&event)?;
+    let encoded: String = general_purpose::STANDARD_NO_PAD.encode(json);
+
+    let mut row = Vec::new();
+    write!(&mut row, "{}\n", encoded)?;
+    Ok(row)
+}
+
+fn decode_event(row: String) -> Event {
+    let trimmed_b64 = row.trim_end();
+    let json_bytes = general_purpose::STANDARD_NO_PAD
+        .decode(trimmed_b64)
+        .expect("Expected row to be decodable from base64");
+    let json = String::from_utf8(json_bytes).expect("Expected row to be valid UTF8");
+
+    let event: Event =
+        serde_json::from_str(&json).expect("Expected row to be deserializable to json");
+    event
+}
+
+#[cfg(test)]
+mod tests {
+    use cloudevents::event::Event;
+    use cloudevents::*;
+    use std::path::PathBuf;
+
+    use crate::db::ExpectedRevision;
+
+    use super::Database;
+
+    struct TestFile(PathBuf);
+
+    impl TestFile {
+        pub fn new(filename: &str) -> Self {
+            let mut tmp = std::env::temp_dir();
+            tmp.push(filename);
+            Self(tmp)
+        }
+
+        pub fn path(&self) -> &PathBuf {
+            &self.0
+        }
+
+        pub fn delete(&self) -> std::io::Result<()> {
+            std::fs::remove_file(&self.0)
+        }
+    }
+
+    impl Drop for TestFile {
+        fn drop(&mut self) {
+            let _ = self.delete();
+        }
+    }
+
+    #[test]
+    fn can_write_and_read() {
+        let test_file = TestFile::new("writereadtest.db");
+        let _ = test_file.delete();
+
+        let mut db = Database::new(test_file.path()).expect("Could not initialize DB");
+
+        let mut event = Event::default();
+
+        db.insert(&mut event, ExpectedRevision::Any).expect("Could not write to the DB");
+
+        let result: Event = db
+            .query(0)
+            .expect("Row not found")
+            .expect("Failed to read row");
+
+        assert_eq!(result.id(), event.id());
+    }
+
+    #[test]
+    fn cannot_write_duplicate_event() {
+        let test_file = TestFile::new("writereadtest.db");
+        let _ = test_file.delete();
+
+        let mut db = Database::new(test_file.path()).expect("Could not initialize DB");
+
+        let mut event = Event::default();
+
+        db.insert(&mut event, ExpectedRevision::Any).expect("Could not write to the DB");
+        assert!(db.insert(&mut event, ExpectedRevision::Any).is_err());
+    }
+
+    #[test]
+    fn read_nonexistent() {
+        let test_file = TestFile::new("readnonexistent.db");
+        let _ = test_file.delete();
+
+        let mut db = Database::new(test_file.path()).expect("Could not initialize DB");
+
+        let result = db.query(0).expect("Expected success reading empty db");
+
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn can_write_expecting_no_stream_in_empty_db() {
+        let test_file = TestFile::new("nostreamemptydb.db");
+        let _ = test_file.delete();
+
+        let mut db = Database::new(test_file.path()).expect("Could not initialize DB");
+
+        let mut event = Event::default();
+
+        db.insert(&mut event, ExpectedRevision::NoStream).expect("Could not write to the DB");
+    }
+
+    #[test]
+    fn cannot_write_expecting_no_stream_in_non_empty_db() {
+        let test_file = TestFile::new("nonemptydbexpectingnostream.db");
+        let _ = test_file.delete();
+
+        let mut db = Database::new(test_file.path()).expect("Could not initialize DB");
+
+        let mut event1 = Event::default();
+        let mut event2 = Event::default();
+        db.insert(&mut event1, ExpectedRevision::NoStream).expect("Could not write to the DB");
+        assert!(db.insert(&mut event2, ExpectedRevision::NoStream).is_err());
+    }
+
+    #[test]
+    fn cannot_write_to_empty_db_expecting_stream_exists() {
+        let test_file = TestFile::new("emptyexpectexists.db");
+        let _ = test_file.delete();
+
+        let mut db = Database::new(test_file.path()).expect("Could not initialize DB");
+
+        let mut event = Event::default();
+
+        assert!(db.insert(&mut event, ExpectedRevision::StreamExists).is_err());
+    }
+
+    #[test]
+    fn cannot_write_expecting_revision_zero_with_empty_db() {
+        let test_file = TestFile::new("expectrevisionzerofailure.db");
+        let _ = test_file.delete();
+
+        let mut db = Database::new(test_file.path()).expect("Could not initialize DB");
+
+        let mut event = Event::default();
+
+        assert!(db.insert(&mut event, ExpectedRevision::Exact(0)).is_err());
+    }
+
+    #[test]
+    fn can_write_expecting_revision_zero_with_present_row() {
+        let test_file = TestFile::new("expectrevisionzerofailure.db");
+        let _ = test_file.delete();
+
+        let mut db = Database::new(test_file.path()).expect("Could not initialize DB");
+
+        let mut event1 = Event::default();
+        let mut event2 = Event::default();
+        db.insert(&mut event1, ExpectedRevision::NoStream).expect("Could not write to the DB");
+        db.insert(&mut event2, ExpectedRevision::Exact(0)).expect("Could not write to the DB");
+    }
+
+    #[test]
+    fn can_write_and_read_many() {
+        let test_file = TestFile::new("writereadmanytest.db");
+        let _ = test_file.delete();
+
+        let mut db = Database::new(test_file.path()).expect("Could not initialize DB");
+
+        let mut event = Event::default();
+
+        for _n in 0..100 {
+            db.insert(&mut Event::default(), ExpectedRevision::Any)
+                .expect("Could not write to the DB");
+        }
+
+        db.insert(&mut event, ExpectedRevision::Any).expect("Could not write to the DB");
+
+        for _n in 0..100 {
+            db.insert(&mut Event::default(), ExpectedRevision::Any)
+                .expect("Could not write to the DB");
+        }
+
+        let result: Event = db
+            .query(100)
+            .expect("Row not found")
+            .expect("Failed to read row");
+
+        assert_eq!(result.id(), event.id());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,82 @@
-use hematite::db::{Database, ExpectedRevision};
-use std::path::PathBuf;
-use cloudevents::event::Event;
+use actix::prelude::*;
+use cloudevents::*;
+use hematite::db::{Database, DatabaseActor, ExpectedRevision, Append, Fetch};
+use std::{collections::HashMap, path::PathBuf};
+use std::sync::RwLock;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-fn main() -> anyhow::Result<()> {
-    let _ = std::fs::remove_file("stream.db");
-    let mut db = Database::new(&PathBuf::from("stream.db"))?;
+type DbActorAddress = actix::Addr<DatabaseActor>;
 
-    db.insert(&mut Event::default(), ExpectedRevision::Any)
+struct AppState {
+    streams: RwLock<HashMap<String, DbActorAddress>>
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/streams/{stream}/events/{rownum}")]
+async fn get_event(state: web::Data<AppState>, stream: web::Path<(String, u64)>) -> impl Responder {
+    let (stream_id, rownum) = stream.into_inner();
+    let streams = state.streams.read().unwrap();
+
+    if let Some(addr) = streams.get(&stream_id) {
+        match addr.send(Fetch(rownum)).await {
+            Ok(Ok(Some(event))) => return HttpResponse::Ok().json(event),
+            Ok(Ok(None)) => return HttpResponse::NotFound().json("Not Found"),
+            Ok(Err(_)) => return HttpResponse::InternalServerError().json("Internal Server Error"),
+            Err(_) => return HttpResponse::InternalServerError().json("Internal Server Error")
+        }
+    } else {
+        return HttpResponse::NotFound().json("Not Found")
+    }
+}
+
+#[post("/streams/{stream}/events")]
+async fn post_event(state: web::Data<AppState>, stream: web::Path<String>, event: web::Json<Event>) -> impl Responder {
+    let stream_id = stream.into_inner();
+
+    let init_db = {
+        let streams = state.streams.read().unwrap();
+
+        !streams.contains_key(&stream_id)
+    };
+
+    if init_db {
+        let db = Database::new(&PathBuf::from(format!("{}.db", stream_id))).unwrap();
+        let addr = DatabaseActor { database: db }.start();
+
+        let mut streams_mut = state.streams.write().unwrap();
+        streams_mut.insert(stream_id.clone(), addr);
+    }
+
+    let streams = state.streams.read().unwrap();
+
+    if let Some(addr) = streams.get(&stream_id) {
+        match addr.send(Append(event.into_inner(), ExpectedRevision::Any)).await {
+            Ok(Ok(())) => return HttpResponse::Created(),
+            Ok(Err(_)) => return HttpResponse::InternalServerError(),
+            Err(_) => return HttpResponse::InternalServerError()
+        }
+    } else {
+        return HttpResponse::InternalServerError()
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let state = web::Data::new(AppState {
+        streams: RwLock::new(HashMap::new())
+    });
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .service(hello)
+            .service(get_event)
+            .service(post_event)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
